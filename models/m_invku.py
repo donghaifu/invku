348ebb35e98f50e357d8f6ebe05fc5133ab8fc35
donghaifu <fudonghai@yeah.net>
2018-06-21 23:18:28 +0800

    隐藏了产品页面的一些按钮和图片，添加了说明，设置了名称的唯一性，取消了一些翻译字段。设置了价格的访问组。完善了工艺BOM零件表.xls 隐藏销售和库存的一些不用菜单

--- models/m_invku.py
+++ models/m_invku.py
@@ -1,3 +1,23 @@
 # -*- coding:utf-8 -*-
 from odoo import models, fields, api
 
+import odoo.addons.decimal_precision as dp
+
+class ProductChange(models.Model):
+	_inherit = 'product.template'
+
+	description = fields.Text(translate=False)
+	description_purchase = fields.Text(translate=False)
+	description_sale = fields.Text(translate=False)
+	description_picking = fields.Text(translate=False)
+
+	name = fields.Char(translate=False)
+	_sql_constraints = [
+		('name_uniq', 'unique(name)', '件号必须唯一!'),
+	 ]
+
+class ProductHidePrice(models.Model):
+	_inherit = 'product.supplierinfo'
+	price = fields.Float(
+		'Price', default=0.0, digits=dp.get_precision('Product Price'),
+		required=True, groups = 'purchase.group_purchase_manager,account.group_account_manager,base.group_system')
