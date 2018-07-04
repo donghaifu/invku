eb7f03db9c4debf9942f4b1f0bc2a288884c183f
donghaifu <fudonghai@yeah.net>
2018-07-04 16:53:10 +0800

    20180714W

--- models/m_invku.py
+++ models/m_invku.py
@@ -16,8 +16,18 @@ class ProductChange(models.Model):
 		('name_uniq', 'unique(name)', '件号必须唯一!'),
 	 ]
 
+class PartnerChange(models.Model):
+	_inherit = 'res.partner'
+
+	_sql_constraints = [
+		('name_uniq', 'unique(name)', '供应商/客户有重名情况发生!'),
+	 ]
+
+
+
+
 class ProductHidePrice(models.Model):
 	_inherit = 'product.supplierinfo'
 	price = fields.Float(
 		'Price', default=0.0, digits=dp.get_precision('Product Price'),
-		required=True, groups = 'purchase.group_purchase_manager,account.group_account_manager,base.group_system')
+		required=True, groups = 'purchase.group_purchase_manager,account.group_account_manager,purchase.group_purchase_user,base.group_system')
